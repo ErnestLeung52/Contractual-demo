839734c3399e6a02a43341f19ac5abf37b4b95e1
Yankun Song <yankun.l.song@gmail.com>
2022-04-23 17:19:44 -0500

    finished backend

---
+++ server/models/commands.sql
@@ -0,0 +1,15 @@
+CREATE TABLE user (
+	name VARCHAR,
+  age INT
+);
+
+
+CREATE TABLE user_balance (
+	name VARCHAR,
+  balance FLOAT
+);
+
+CREATE TABLE history (
+	name VARCHAR,
+  change FLOAT
+);
